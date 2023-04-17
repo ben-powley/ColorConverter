3e03d7c910b069a03bbe11eb283ba1fb94f4c679
Ben Powley <ben.powley@ubisoft.com>
2023-04-17 11:18:39 +0100

    Add conversion

---
+++ .prettierrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  plugins: [require("prettier-plugin-tailwindcss")],
+  semi: false,
+  tabWidth: 2,
+  printWidth: 150,
+  singleQuote: true,
+  trailingComma: "es5",
+  bracketSameLine: true,
+  useTabs: false,
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,103 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
+import { hexToRgb, rgbToHex } from './Helpers/ColorHelper'
+
+type ConvertMode = 'hextorgb' | 'rgbtohex'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [convertMode, setConvertMode] = useState<ConvertMode>('hextorgb')
+  const [colorString, setColorString] = useState('')
+
+  const [rString, setRString] = useState('')
+  const [gString, setGString] = useState('')
+  const [bString, setBString] = useState('')
+
+  const [convertResult, setConvertResult] = useState('')
+
+  useEffect(() => {
+    switch (convertMode) {
+      default:
+      case 'hextorgb':
+        if (colorString.length > 5) {
+          const result = hexToRgb(colorString)
+
+          if (result) {
+            setConvertResult(`${result.r}, ${result.g}, ${result.b}`)
+          }
+        }
+        break
+
+      case 'rgbtohex':
+        if (rString && gString && bString) {
+          const result = rgbToHex({ r: parseInt(rString), g: parseInt(gString), b: parseInt(bString) })
+
+          if (result) {
+            setConvertResult(`${result}`)
+          }
+        }
+
+        break
+    }
+  }, [convertMode, colorString, rString, gString, bString])
+
+  useEffect(() => {
+    setConvertResult('')
+  }, [convertMode])
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="my-12 flex flex-col items-center justify-center font-bold">
+      <div className="text-4xl">Convert</div>
+
+      <div className="mt-4 flex items-center gap-x-3">
+        <select className="h-[36px] rounded-md border-2 border-gray-300 px-2 py-1" onChange={(e) => setConvertMode(e.target.value as ConvertMode)}>
+          <option value={'hextorgb'}>HEX -&gt; RGB</option>
+          <option value={'rgbtohex'}>RGB -&gt; HEX</option>
+        </select>
+
+        <div>
+          {convertMode === 'hextorgb' ? (
+            <input
+              className="h-[36px] rounded-md border-2 border-gray-300 px-2 py-1"
+              placeholder="Color"
+              onChange={(e) => setColorString(e.target.value)}
+            />
+          ) : (
+            <div className="flex items-center gap-x-2">
+              <input
+                className="h-[36px] w-[60px] rounded-md border-2 border-gray-300 px-2 py-1"
+                placeholder="R"
+                onChange={(e) => setRString(e.target.value)}
+                maxLength={3}
+              />
+              <input
+                className="h-[36px] w-[60px] rounded-md border-2 border-gray-300 px-2 py-1"
+                placeholder="G"
+                onChange={(e) => setGString(e.target.value)}
+                maxLength={3}
+              />
+              <input
+                className="h-[36px] w-[60px] rounded-md border-2 border-gray-300 px-2 py-1"
+                placeholder="B"
+                onChange={(e) => setBString(e.target.value)}
+                maxLength={3}
+              />
+            </div>
+          )}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      {convertResult && (
+        <div className="mt-12 flex flex-col items-center text-4xl">
+          <div className="flex items-center gap-x-4">
+            <div
+              className="h-16 w-16"
+              style={{ backgroundColor: `${convertMode === 'hextorgb' ? `rgb(${convertResult})` : `${convertResult}`}` }}></div>
+            <div>{convertResult}</div>
+          </div>
+          <button className="mt-3 rounded-md bg-gray-200 px-3 py-1 text-base" onClick={() => navigator.clipboard.writeText(convertResult)}>
+            Copy
+          </button>
+        </div>
+      )}
     </div>
   )
 }
